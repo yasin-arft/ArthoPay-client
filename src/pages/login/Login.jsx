d24469291767f6013bfe080cdd745f708e756c77
yasin-arft <yasinarftsocial@gmail.com>
2024-07-16 16:15:00 +0600

    basic login form setup

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,84 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+const loginSchema = z.object({
+  emailOrPhone: z.string().min(2).max(50),
+  pin: z.string().min(2).max(50),
+})
+
 const Login = () => {
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values) {
+    console.log(values)
+  }
+
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <main className="container px-3 max-w-screen-xl mx-auto">
+      <section className="w-full h-screen flex justify-center items-center">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-center">Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+                <FormField
+                  control={form.control}
+                  name="emailOrPhone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email or Phone</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email or phone number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="pin"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Pin</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your pin" {...field} type="number" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className='w-full'>Login</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </section>
+    </main>
   );
 };
 
