9880f9a4e43b6387ab4e1e50b4ff32c06cc4f82d
yasin-arft <yasinarftsocial@gmail.com>
2024-07-16 12:20:19 +0600

    shadcn setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <h1 className="text-3xl font-bold underline">
       Hello world!
+      <Button>Click me</Button>
     </h1>
   </React.StrictMode>,
 )
