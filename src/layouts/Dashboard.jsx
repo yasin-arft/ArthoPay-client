8b0478ec48fc0ad96220c9aa998a4fba868b4c6d
yasin-arft <yasinarftsocial@gmail.com>
2024-07-17 22:01:56 +0600

    dashboard layout setup

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+  
   return (
     <div>
-      <header>
-        Hello world!
+      <header className="bg-primary text-primary-foreground">
+        <nav className="container px-3 max-w-screen-xl mx-auto flex justify-between items-center">
+          <h2 className="lg:text-3xl md:text-2xl text-xl p-2 font-bold">Artho Pay</h2>
+          <Button variant="secondary">Logout</Button>
+        </nav>
       </header>
+      <main className="container px-3 max-w-screen-xl mx-auto">
+        <Outlet />
+      </main>
     </div>
   );
 };
